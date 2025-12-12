e70b01ce4977537b9ff08e25b33c519eb081b19e
Jonathan Guzmán <jonguz5@hotmail.com>
2025-12-11 21:15:14 -0600

    function placeholders

--- RayC/include/ray/compiler/passes/typeScanner.hpp
+++ RayC/include/ray/compiler/passes/typeScanner.hpp
@@ -23,5 +23,36 @@ class TypeScanner : public ast::StatementVisitor,
 	bool hasFailed() const;
 	const std::vector<std::string> getErrors() const;
 	const std::vector<std::string> getWarnings() const;
+
+  private:
+	void visitBlockStatement(const ast::Block &value) override;
+	void visitTerminalExprStatement(const ast::TerminalExpr &value) override;
+	void
+	visitExpressionStmtStatement(const ast::ExpressionStmt &value) override;
+	void visitFunctionStatement(const ast::Function &value) override;
+	void visitIfStatement(const ast::If &value) override;
+	void visitJumpStatement(const ast::Jump &value) override;
+	void visitVarDeclStatement(const ast::VarDecl &value) override;
+	void visitMemberStatement(const ast::Member &value) override;
+	void visitWhileStatement(const ast::While &value) override;
+	void visitStructStatement(const ast::Struct &value) override;
+	void visitCompDirectiveStatement(const ast::CompDirective &value) override;
+	// Expression
+	void visitVariableExpression(const ast::Variable &value) override;
+	void visitIntrinsicExpression(const ast::Intrinsic &value) override;
+	void visitAssignExpression(const ast::Assign &value) override;
+	void visitBinaryExpression(const ast::Binary &value) override;
+	void visitCallExpression(const ast::Call &value) override;
+	void visitIntrinsicCallExpression(const ast::IntrinsicCall &value) override;
+	void visitGetExpression(const ast::Get &value) override;
+	void visitGroupingExpression(const ast::Grouping &value) override;
+	void visitLiteralExpression(const ast::Literal &value) override;
+	void visitLogicalExpression(const ast::Logical &value) override;
+	void visitSetExpression(const ast::Set &value) override;
+	void visitUnaryExpression(const ast::Unary &value) override;
+	void visitArrayAccessExpression(const ast::ArrayAccess &value) override;
+	void visitTypeExpression(const ast::Type &value) override;
+	void visitCastExpression(const ast::Cast &value) override;
+	void visitParameterExpression(const ast::Parameter &value) override;
 };
 } // namespace ray::compiler::passes
